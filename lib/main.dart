eac318a7c762b93e4e788d0668634558bcc9c814
ben-lawino <lawinwes@gmail.com>
2024-10-02 13:49:03 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,57 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<String> _measures  = [
+    'meters',
+    'kilometers',
+    'grams',
+    'kilograms',
+    'feet',
+    'miles',
+    'pounds(lbs)',
+    'ounces',
+  ];
+   double? _numberFrom;
+  @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Measures Converter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Measures Converter'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              DropdownButton(items: _measures.map((String value){
+                return DropdownMenuItem<String>(value :value,child: Text(value),);
+        },).toList(),
+            onChanged:(_){},),
+              TextField(
+                onChanged: (text){
+                  var rv =double.tryParse(text);
+                  if(rv != null ) {
+                    setState(() {
+                      _numberFrom = rv;
+                    });
+                  }
+                },
+              ),
+              Text((_numberFrom == null)?'':_numberFrom.toString())
+            ],
+          ),
+
+        ),
+      ),
     );
   }
 }
-
